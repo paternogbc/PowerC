95c5adacac1f2597f74d5eb0b21b18ecbcb15c45
Gijsbert Werner <g.werner@vu.nl>
2014-12-21 15:47:49 +0100

    Simplief trait generation (check history log, for older more complex one, based on Gustavo's trait simulations).

--- influence.corhmm.R
+++ influence.corhmm.R
@@ -13,14 +13,10 @@ N <- 100 # Number of species
 ### Simulating tree
 tree<-pbtree(n=N)    
 plot(tree)
-### Simulating response variable with phylogenetic signal
-Ly<-rTrait(n=1, tree, model=c("lambda"),parameters=list(lambda=.8)) #Use exact same simulations as previously as basis
 
 #Everything above this, doesn't need to be redone, it's already in Gustavo's code
-X<-cbind(rep(1,100),Ly)
-y<-rbinTrait(n=1, phy=tree,beta=c(-1,0.5), alpha=1, X=X) #From the help-page
-#I still need to consider this function (rbintrait) in more detail, to understand it better.
-#Other option: y<-rbinTrait(n=1, phy=tree,beta=c(0.7),alpha=1)
+#Simulate binary trait with phylogenetic signal. #Also consider using the (primates data from corHMM) 
+y<-rbinTrait(n=1, phy=tree,beta=c(0.5),alpha=1)
 table(y)
 #Plot the binary states (in red/blue on the phylogeny)
 plot.phylo(tree,tip.color = c("blue","red")[factor(y)])
