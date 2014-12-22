c8706c6f5d9ede64caf16f81d8a5bec438d03946
paternogbc <paternogbc@gmail.com>
2014-12-22 09:47:08 -0300

    new function
    Estimate time of simulations

---
+++ powerCtime.R
@@ -0,0 +1,8 @@
+## sampling.pgls: Analysis of sampling effort for PGLS linear regression
+## Author: Gustavo Paterno (paternogbc@gmail.com)
+## Date created: 10.12.14
+
+## This code is not totally checked, please be aware!
+
+## Load required packages:
+library(caper)
